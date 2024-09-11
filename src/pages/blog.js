ef3cb02eebd63d0b58b7f43869bd93e37e60211a
diazmc <mc.arthur_d@hotmail.com>
2024-09-12 00:18:32 +0800

    render blogs using mdx

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -6,24 +6,30 @@ import { graphql } from "gatsby"
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="Blog Page">
-      <ul>
-        {
-          data.allFile.nodes
-            .sort((a, b) => a.name.localeCompare(b.name))
-            .map(node => (
-              <li key={node.name}>{node.name}</li>
-            ))
-        }
-      </ul>
+      {
+        data.allMdx.nodes
+          .map(node => (
+            <article key={node.id}>
+              <h2>{node.frontmatter.title}</h2>
+              <p>Posted: {node.frontmatter.date}</p>
+              <p>{node.excerpt}</p>
+            </article>
+          ))
+      }
     </Layout>
   )
 }
 
 export const query = graphql`
   query {
-    allFile(filter: {sourceInstanceName: {eq: "blog"}}) {
+    allMdx {
       nodes {
-        name
+        frontmatter {
+          title
+          date(formatString: "MMMM D, YYYY")
+        }
+        id
+        excerpt
       }
     }
   }
